24169b2a63cbf5c88911415f6479dab410f582cc
Kartik <kartik.arora1214@gmail.com>
2020-12-02 12:21:14 +0530

    node tools for fetching data

--- tools/sheets.js
+++ tools/sheets.js
@@ -10,6 +10,8 @@ let sheets = google.sheets({
 
 let debug = true;
 let qualifiers128_backup = require("./qualifiers128.json");
+let qualifiers64_backup = require("./qualifiers64.json");
+let qualifiers32_backup = require("./qualifiers32.json");
 let teams_backup = require("./allPlayerData.json");
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
@@ -19,7 +21,7 @@ async function getAllPlayers() {
     sheets.spreadsheets.values
       .get({
         spreadsheetId: "1I3cnl0rgKrwoymq82MhHXg_SasBPbsvqbfRnGgdI11U",
-        range: "Qualifier Teams!B5:H135",
+        range: "Qualifier Teams!B5:J",
       })
       .then((sheetVal) => {
         console.log(sheetVal.data);
@@ -53,13 +55,55 @@ async function getAllQualifiers128() {
   }
 }
 
+async function getAllQualifiers64() {
+  //Round of 64
+  if (!debug) {
+    sheets.spreadsheets.values
+      .get({
+        spreadsheetId: "1I3cnl0rgKrwoymq82MhHXg_SasBPbsvqbfRnGgdI11U",
+        range: "Qualifiers!K8:Q",
+      })
+      .then((sheetVal) => {
+        console.log(sheetVal.data);
+        fs.writeFileSync("./qualifiers64.json", JSON.stringify(sheetVal.data));
+        qualifiers64ToMD(sheetVal.data);
+      });
+  } else {
+    let data = qualifiers64_backup;
+    console.log(data.range);
+    qualifiers64ToMD(data);
+  }
+}
+
+
+async function getAllQualifiers32() {
+  //Round of 32
+  if (!debug) {
+    sheets.spreadsheets.values
+      .get({
+        spreadsheetId: "1I3cnl0rgKrwoymq82MhHXg_SasBPbsvqbfRnGgdI11U",
+        range: "Qualifiers!T14:Z",
+      })
+      .then((sheetVal) => {
+        console.log(sheetVal.data);
+        fs.writeFileSync("./qualifiers32.json", JSON.stringify(sheetVal.data));
+        qualifiers32ToMD(sheetVal.data);
+      });
+  } else {
+    let data = qualifiers32_backup;
+    console.log(data.range);
+    qualifiers32ToMD(data);
+  }
+}
+
 async function qualifiers128ToMD(data) {
   let qualifiers128 = data.values;
   let totalLength = qualifiers128.length;
   console.log({ totalLength });
   for (i = 0; i < totalLength; i += 6) {
     let data = "";
-
+    let filename = qualifiers128[i][0].split(" #").join("_");
+    console.log(qualifiers128[i+2]);
     // converting team names into links to team pages
     qualifiers128[i+2][0] = `[${qualifiers128[i+2][0]}](/teams/${qualifiers128[i+2][0].split(' ').join('_').toLowerCase()})`;
     qualifiers128[i+3][0] = `[${qualifiers128[i+3][0]}](/teams/${qualifiers128[i+3][0].split(' ').join('_').toLowerCase()})`;
@@ -69,6 +113,9 @@ async function qualifiers128ToMD(data) {
       team_1: qualifiers128[i + 2][0],
       team_2: qualifiers128[i + 3][0],
       title: qualifiers128[i][0],
+      optval: filename,
+      type: 'qualifier',
+      round: 'Round of 128'
     });
 
     data += YAML.stringify({
@@ -83,7 +130,86 @@ async function qualifiers128ToMD(data) {
     data += `| ${qualifiers128[i + 2].join(" | ")} |\n`;
     data += `| ${qualifiers128[i + 3].join(" | ")} |\n`;
 
-    let filename = qualifiers128[i][0].split(" #").join("_");
+
+    console.log(filename);
+    fs.writeFileSync(`../_qualifiers/${filename}.md`, data);
+  }
+}
+
+async function qualifiers64ToMD(data) {
+  let qualifiers64 = data.values;
+  let totalLength = qualifiers64.length;
+  console.log({ totalLength });
+  for (i = 0; i < totalLength; i += 12) {
+    let data = "";
+    let filename = qualifiers64[i][0].split(" #").join("_");
+    // converting team names into links to team pages
+    qualifiers64[i+2][0] = `[${qualifiers64[i+2][0]}](/teams/${qualifiers64[i+2][0].split(' ').join('_').toLowerCase()})`;
+    qualifiers64[i+3][0] = `[${qualifiers64[i+3][0]}](/teams/${qualifiers64[i+3][0].split(' ').join('_').toLowerCase()})`;
+
+    data += `---\n`;
+    data += YAML.stringify({
+      team_1: qualifiers64[i + 2][0],
+      team_2: qualifiers64[i + 3][0],
+      title: qualifiers64[i][0],
+      optval: filename,
+      type: 'qualifier',
+      round: 'Round of 64'
+    });
+
+    data += YAML.stringify({
+      status: qualifiers64[i + 2][1] === "-" ? "TBP" : "Concluded",
+      excerpt: `${qualifiers64[i + 2][0]} vs ${qualifiers64[i + 3][0]}`,
+    });
+    data += `\n---\n`;
+
+    data += `## Results\n\n`;
+    data += `| ${qualifiers64[i + 1].join(" | ")} |\n`;
+    data += `| -- | -- | -- | -- | -- | -- | -- |\n`;
+    data += `| ${qualifiers64[i + 2].join(" | ")} |\n`;
+    data += `| ${qualifiers64[i + 3].join(" | ")} |\n`;
+
+
+    console.log(filename);
+    fs.writeFileSync(`../_qualifiers/${filename}.md`, data);
+  }
+}
+
+
+async function qualifiers32ToMD(data) {
+  let qualifiers32 = data.values;
+  let totalLength = qualifiers32.length;
+  console.log({ totalLength });
+  for (i = 0; i < totalLength; i += 24) {
+    let data = "";
+    let filename = qualifiers32[i][0].split(" #").join("_");
+    // converting team names into links to team pages
+    qualifiers32[i+2][0] = `[${qualifiers32[i+2][0]}](/teams/${qualifiers32[i+2][0].split(' ').join('_').toLowerCase()})`;
+    qualifiers32[i+3][0] = `[${qualifiers32[i+3][0]}](/teams/${qualifiers32[i+3][0].split(' ').join('_').toLowerCase()})`;
+
+    data += `---\n`;
+    data += YAML.stringify({
+      team_1: qualifiers32[i + 2][0],
+      team_2: qualifiers32[i + 3][0],
+      title: qualifiers32[i][0],
+      optval: filename,
+      type: 'qualifier',
+      round: 'Round of 32'
+    });
+
+    data += YAML.stringify({
+      status: qualifiers32[i + 2][1] === "-" ? "TBP" : "Concluded",
+      excerpt: `${qualifiers32[i + 2][0]} vs ${qualifiers32[i + 3][0]}`,
+    });
+    data += `\n---\n`;
+
+    data += `## Results\n\n`;
+    data += `| ${qualifiers32[i + 1].join(" | ")} |\n`;
+    data += `| -- | -- | -- | -- | -- | -- | -- |\n`;
+    data += `| ${qualifiers32[i + 2].join(" | ")} |\n`;
+    data += `| ${qualifiers32[i + 3].join(" | ")} |\n`;
+
+
     console.log(filename);
     fs.writeFileSync(`../_qualifiers/${filename}.md`, data);
   }
@@ -99,21 +225,23 @@ async function playerDataToMD(data) {
     data += `---\n`;
     data += YAML.stringify({
 
-      title: eachTeam[1],
-      player_1: eachTeam[2],
-      player_2: eachTeam[3],
-      match_difference: eachTeam[5],
-      map_difference: eachTeam[6]
+      title: eachTeam[3],
+      player_1: eachTeam[4],
+      player_2: eachTeam[5],
+      match_difference: eachTeam[7],
+      map_difference: eachTeam[8],
+      seed: eachTeam[0],
+      average_elo: eachTeam[1].replace(',','.')
     });
-    if (eachTeam[4] !== "") {
+    if (eachTeam[6] !== "") {
       data += YAML.stringify({
 
-        subsitute: eachTeam[4],
-        excerpt: `${eachTeam[2]}, ${eachTeam[3]} and ${eachTeam[4]}`
+        subsitute: eachTeam[6],
+        excerpt: `${eachTeam[4]}, ${eachTeam[5]} and ${eachTeam[6]}`
       });
     } else {
       data += YAML.stringify({
-        excerpt: `${eachTeam[2]} and ${eachTeam[3]}`
+        excerpt: `${eachTeam[4]} and ${eachTeam[5]}`
       });
     }
 
@@ -125,10 +253,10 @@ async function playerDataToMD(data) {
     data += `## Players\n\n`;
     data += `| Player 1 | Player 2 | Subsitute |\n`;
     data += `| -- | -- | -- |\n`;
-    data += `| ${eachTeam[2]} | ${eachTeam[3]} | ${eachTeam[4] === "" ? "NIL" : eachTeam[4]} |\n`;
+    data += `| ${eachTeam[4]} | ${eachTeam[5]} | ${eachTeam[6] === "" ? "NIL" : eachTeam[6]} |\n`;
 
 
-    let filename = eachTeam[1].split(" ").join("_").toLowerCase();
+    let filename = eachTeam[3].split(" ").join("_").toLowerCase();
     console.log(filename);
     fs.writeFileSync(`../_teams/${filename}.md`, data);
   })
@@ -137,3 +265,5 @@ async function playerDataToMD(data) {
 
 getAllPlayers().catch(console.error);
 getAllQualifiers128().catch(console.error);
+getAllQualifiers64().catch(console.error);
+getAllQualifiers32().catch(console.error);
